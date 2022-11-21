ec94fa5f7b4e2f0b3c5006e49f2d5c186508a957
izik <izik4004@gmail.com>
2022-11-21 15:34:19 +0100

    project setup

---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {nav} from "../data"
+
+const Nav = () => {
+  return <nav>
+    <ul className='flex gap-x-10'>
+      {nav.map((item, index) => {
+        const {href, name} = item;
+        return <li key={index}>
+          <a className='hover:text-accent transition' href={href}>{name}</a>
+        </li>
+      })}
+    </ul>
+  </nav>;
+};
+
+export default Nav;
---
+++ src/data.js
@@ -0,0 +1,11 @@
+export const header = {
+    // logo: LogoImg,
+    btnText: 'Request a demo',
+  };
+
+export const nav = [
+    { name: 'Features', href: '/' },
+    { name: 'About us', href: '/' },
+    { name: 'Pricing', href: '/' },
+    { name: 'Feedback', href: '/' },
+  ];
