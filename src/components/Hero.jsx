fd331d14ea31df0df0afc0fafc146fbb80ce3040
izik <izik4004@gmail.com>
2022-11-25 16:36:09 +0100

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { hero } from "../data/data";
 import { HiOutlineChevronDown } from "react-icons/hi";
 import SellForm from "./SellForm";
-import Image from "../assets/svg/casual.png"
+import Image from "../assets/svg/casual.png";
 // import SellForm from "./SellForm";
 
 const Hero = () => {
@@ -10,7 +10,6 @@ const Hero = () => {
   const { title, subtitle, btnText, compText, image } = hero;
   return (
     <>
-     
       {/* <div
         aria-hidden="true"
         class="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20"
@@ -20,26 +19,56 @@ const Hero = () => {
       </div> */}
 
       <section className="flex items-center container justify-between  me  mx-auto rounded-xl min-h-[700px]">
-      
-      <section class=" dark:bg-gray-900">
-        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        {modalOpen === true ? (
+          <SellForm setModalOpen={setModalOpen} modalOpen={modalOpen} />
+        ) : (
+          <></>
+        )}
+        <section class=" dark:bg-gray-900">
+          <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
             <div class="mr-auto place-self-center lg:col-span-7">
-                <h1 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white">Payments tool for software companies</h1>
-                <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.</p>
-                <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                    Get started
-                    <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                </a>
-                <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                    Speak to Sales
-                </a> 
+              <h1 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white">
+                Afraid of <span className="text-accent">getting scammed </span>{" "}
+                by online vendors?
+              </h1>
+              <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                From checkout to global sales tax compliance, companies around
+                the world use Flowbite to simplify their payment stack.
+              </p>
+              <a onClick={() => setModalOpen(true)}
+                href="#"
+                class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+              >
+                Get started
+                <svg
+                  class="w-5 h-5 ml-2 -mr-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+              <a
+                href="#"
+                class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+              >
+                Speak to Sales
+              </a>
             </div>
             <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-                <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup"/>
-            </div>                
-        </div>
-    </section>
-      {/* <div className="flex px-10 gap-6">
+              <img
+                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png"
+                alt="mockup"
+              />
+            </div>
+          </div>
+        </section>
+        {/* <div className="flex px-10 gap-6">
         <div className="bg-red-500 w-1/2">
         <h1
                 className="text-5xl italic font-bold mb-2 lg:mb-5 text-center"
@@ -52,8 +81,7 @@ const Hero = () => {
         </div>
         <div className="w-1/3">3ddddddddddd</div>
       </div> */}
-      
-      
+
         {/* <div
           className="mx-auto  w-2/3
       flex justify-center items-center"
@@ -102,7 +130,6 @@ const Hero = () => {
           </div>
           </div>
         </div> */}
-        
       </section>
     </>
   );
--- src/components/SellForm.jsx
+++ src/components/SellForm.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const SellForm = ({modalOpen, setModalOpen}) => {
   return (
-    <div class="py-12 bg-gray-700 transition duration-150 ease-in-out z-10 absolute top-0 right-0 bottom-0 left-0" id="modal">
-    <div role="alert" class="container mx-auto w-11/12 md:w-2/3 max-w-lg">
-        <div class="relative py-8 px-5 md:px-10 bg-white shadow-md rounded border border-gray-400">
+    <div 
+    show={modalOpen}
+    // onHide={() => setItemModal(false)}
+    class="py-12 bg-gray-100 transition duration-50 ease-in-out z-10 absolute top-0 right-0 bottom-0 left-0" id="modal">
+    <div role="alert" class=" mx-auto w-8/12 md:w-2/3 max-w-md">
+        <div class="relative py-8 px-5 md:px-10 bg-white shadow-md rounded-xl">
             <div  class="w-full flex justify-start text-gray-600 mb-3">
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-wallet" width="52" height="52" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" />
@@ -58,7 +61,7 @@ const SellForm = ({modalOpen, setModalOpen}) => {
                 <button class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 bg-indigo-700 rounded text-white px-8 py-2 text-sm">Submit</button>
                 <button class="focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-400 ml-3 bg-gray-100 transition duration-150 text-gray-600 ease-in-out hover:border-gray-400 hover:bg-gray-300 border rounded px-8 py-2 text-sm"  onClick={() =>setModalOpen(false)} >Cancel</button>
             </div>
-            <button class="cursor-pointer absolute top-0 right-0 mt-4 mr-5 text-gray-400 hover:text-gray-600 transition duration-150 ease-in-out rounded focus:ring-2 focus:outline-none focus:ring-gray-600" onclick="modalHandler()" aria-label="close modal" role="button">
+            <button onClick={() => setModalOpen(false)} class="cursor-pointer absolute top-0 right-0 mt-4 mr-5 text-gray-400 hover:text-gray-600 transition duration-150 ease-in-out rounded focus:ring-2 focus:outline-none focus:ring-gray-600" onclick="modalHandler()" aria-label="close modal" role="button">
                 <svg  xmlns="http://www.w3.org/2000/svg"  class="icon icon-tabler icon-tabler-x" width="20" height="20" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" />
                     <line x1="18" y1="6" x2="6" y2="18" />
