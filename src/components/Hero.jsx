a0685dfb913fa4b3712dac2fcd386b0ba285c02a
izik <izik4004@gmail.com>
2022-11-26 02:24:59 +0100

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -38,11 +38,11 @@ const Hero = () => {
                 href="#"
                 className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
               >
-                SELL
+                Sell your products
               </a>
               <a
                 href="#"
-                className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+                className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center  border border-accent rounded-lg hover:bg-gray-100 focus:ring-4 "
               >
                Shop Now
               </a>
